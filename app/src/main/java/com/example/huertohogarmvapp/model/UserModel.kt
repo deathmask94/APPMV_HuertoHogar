2d448a2134ff1bcd35bf50660b824c44b4b5606b
Francisco Ortega <franci.ortegac@duocuc.cl>
2025-10-21 11:30:50 -0300

    commit 3

--- app/src/main/java/com/example/huertohogarmvapp/model/UserModel.kt
+++ app/src/main/java/com/example/huertohogarmvapp/model/UserModel.kt
@@ -1,7 +1,11 @@
 package com.example.huertohogarmvapp.model
 
+
+
 data class UserModel(
-    val name : String,
-    val email : String,
-    val udi : String
+    val name : String = "" ,
+    val email : String = "",
+    val udi : String = "",
+    val cartItems : Map<String,Long> = mapOf(),
+    val address : String = "",
 )
--- app/src/main/java/com/example/huertohogarmvapp/pages/ProfilePage.kt
+++ app/src/main/java/com/example/huertohogarmvapp/pages/ProfilePage.kt
@@ -1,10 +1,109 @@
 package com.example.huertohogarmvapp.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.example.huertohogarmvapp.model.UserModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.R.attr.fontWeight
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.res.painterResource
+
+import com.example.huertohogarmvapp.R
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.TextField
+import androidx.navigation.NavHostController
+
 
 @Composable
 fun ProfilePage (modifier: Modifier = Modifier){
-    Text(text = "perfil")
+
+    val userModel = remember {
+        mutableStateOf(UserModel())
+    }
+
+    var addressInput by remember {
+        mutableStateOf(userModel.value.address)
+    }
+
+
+    LaunchedEffect(key1 = Unit) {
+        Firebase.firestore.collection("user")
+            .document(FirebaseAuth.getInstance().currentUser?.uid!!)
+            .get().addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val result = it.result.toObject(UserModel::class.java)
+                    if (result != null){
+                        userModel.value = result
+                        addressInput = userModel.value.address
+                    }
+                }
+            }
+    }
+
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(17.dp),
+    ) {
+        Text(
+            text = ("Tu Perfil"), style = TextStyle(
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "fotoperfil",
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        )
+
+        Text(
+            text = userModel.value.name,
+            fontSize = 27.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+            )
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Direccion: ",
+            fontSize = 17.sp,
+            fontWeight = FontWeight.Medium,
+        )
+
+
+
+    }
 }
